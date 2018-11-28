f1f1b4f8fae8baa6fc80d8ce7682d2755ee8316e
JoshTyrrellBrowne <C00224660@itcarlow.ie>
2018-11-28 17:13:59 +0000

    triangle strip added

--- SFMLOpenGL/Game.cpp
+++ SFMLOpenGL/Game.cpp
@@ -2,6 +2,11 @@
 
 Game::Game() : window(VideoMode(800, 600), "OpenGL")
 {
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f); 
+	glMatrixMode(GL_PROJECTION); 
+	glLoadIdentity(); 
+	gluPerspective(45.0, window.getSize().x / window.getSize().y, 1.0, 500.0); 
+	glMatrixMode(GL_MODELVIEW);
 }
 
 Game::~Game() {}
@@ -43,6 +48,25 @@ void Game::update()
 void Game::draw()
 {
 	cout << "Draw up" << endl;
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
+	/*glBegin(GL_TRIANGLES); 
+	{
+		glVertex3f(0.0, 2.0, -5.0);
+		glVertex3f(-2.0, -2.0, -5.0);
+		glVertex3f(2.0, -2.0, -5.0);
+	}
+	glEnd(); */
+
+	glBegin(GL_TRIANGLE_STRIP);
+	{
+		glVertex3f(0.0, 0.0, -10.0);
+		glVertex3f(12.0, 20.0, -10.0);
+		glVertex3f(16.0, 60.0, -10.0);
+		glVertex3f(28.0, 90.0, -10.0);
+	}
+	glEnd();
+	
+	window.display();
 }
 
 void Game::unload()
